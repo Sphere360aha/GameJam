91dac320578a3c7062907efd98d390354faf9f3b
HilalMuhamed <sphere360aha2023@gmail.com>
2023-08-24 18:34:09 +0530

    Enemy1 added

---
+++ Sphere/Assets/Hilal/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public Rigidbody2D rb;
+    public float speed = 3f;
+    public float rotspeed =200f;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 direction = target.position - transform.position;
+        direction.Normalize();
+        float rot = Vector3.Cross(direction,transform.up).z;
+        rb.angularVelocity = -rot * rotspeed;
+        rb.velocity = transform.up * speed;
+    }
+}
+
